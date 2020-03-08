55b611d6891e96356599c023f9a0343ece71dae5
Aldo Escobar <aldo.ea@hotmail.com>
2020-03-08 08:10:28 -0600

    Test on rasp master.py modifications

--- hardware/raspberry/master.py
+++ hardware/raspberry/master.py
@@ -1,19 +1,59 @@
 import serial, string, time
-ser=serial.Serial('/dev/ttyACM0', 9600)
 #The following block of code works like this:
 #If serial data is present, read the line, decode the UTF8 data,
 #...remove the trailing end of line characters
 #...split the data into temperature and humidity
 #...remove the starting and ending pointers (< >)
 #...print the output
-while True:
-        if ser.in_waiting > 0:
-            rawserial = ser.readline()
-            cookedserial = rawserial.decode('utf-8').strip('\r\n')
-            datasplit = cookedserial.split(',')
-            temperature = datasplit[0].strip('<')
-            humidity = datasplit[1]
-            illumination = datasplit[2].strip('>')
-            print('temperature', temperature)
-            print('humidity', humidity)
-            print('illumination', illumination)
+def sleep(secs):
+    time.sleep(secs)
+
+# Python program to get average of a list 
+def average(lst): 
+	return sum(lst) / len(lst) 
+
+def triggerDataPost(tempBuffer, humidityBuffer, illuminationBuffer):
+    tempAvg = average(tempBuffer)
+    humidityAvg = average(humidityAvg)
+    illuminationAvg = average(illuminationBuffer)
+    print(f"=> Temperatura: {tempAvg}, Humedad: {humidityAvg}, Iluminacion: {illuminationAvg}")
+
+def main():
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    recordCounter = 0
+    serialConnectionAttemps = 0
+    tempBuffer = []
+    humidityBuffer = []
+    illuminationBuffer = []
+    while True:
+        try:
+                if ser.in_waiting > 0:
+                    serialConnectionAttemps = 0
+                    rawserial = ser.readline()
+                    cookedserial = rawserial.decode('utf-8').strip('\r\n')
+                    datasplit = cookedserial.split(',')
+                    temperature = datasplit[0].strip('<')
+                    humidity = datasplit[1]
+                    illumination = datasplit[2].strip('>')
+                    tempBuffer.append(temperature)
+                    humidityBuffer.append(humidity)
+                    illuminationBuffer.append(illumination)
+                    if recordCounter == 5 and len(tempBuffer) == 5 and len(humidityBuffer) == 5 and len(illuminationBuffer) == 5:
+                        triggerDataPost()
+                        tempBuffer = []
+                        humidityBuffer = []
+                        illuminationBuffer = []
+                        recordCounter = 0
+                else:
+                    print('=> No serial stream detected, waiting 3 secs...')
+                    sleep(3)
+                    serialConnectionAttemps+=1
+                    if serialConnectionAttemps > 100: break
+        except expression as identifier:
+                # Wait for 5 seconds
+                print("=========== ERROR ===========")
+                print(identifier)
+                sleep(5)
+
+if __name__ == "__main__":
+    main()
